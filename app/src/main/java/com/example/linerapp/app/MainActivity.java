dd271d84f22c765efefefcb3110563a91c46f263
Stas <stasssm@mail.ru>
2014-06-22 12:06:20 +0700

    работающая карта

--- app/src/main/java/com/example/linerapp/app/MainActivity.java
+++ app/src/main/java/com/example/linerapp/app/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends Activity implements OnClickListener {
 
         }
     }
-
+    //комент
     @Override
     public void onBackPressed() {
         Log.e("Exit", "Exit");
