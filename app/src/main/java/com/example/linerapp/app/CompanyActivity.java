f222f73acc3469ed3807aa0a57673c1f29f0ba12
IlnarH <downfallfast@gmail.com>
2014-06-25 01:38:54 +0400

    Переработан JSONLoader

--- app/src/main/java/com/example/linerapp/app/CompanyActivity.java
+++ app/src/main/java/com/example/linerapp/app/CompanyActivity.java
@@ -1,7 +1,9 @@
 package com.example.linerapp.app;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -12,6 +14,11 @@ import com.example.linerapp.app.model.Company;
 import com.example.linerapp.app.util.CompanyListAdapter;
 import com.example.linerapp.app.util.JSONLoader;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
@@ -19,56 +26,63 @@ import java.util.concurrent.ExecutionException;
 
 public class CompanyActivity extends Activity {
 
-    public static CompanyActivity INSTANCE;
-    List<Category> categoryJsonArray;
-    List<Company> companyJsonArray;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        INSTANCE = this;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.company_layout);
 
-        JSONLoader jsonLoader = new JSONLoader();
-        try {
-            categoryJsonArray = (List<Category>) jsonLoader.execute(Category.class).get();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        jsonLoader = new JSONLoader();
-        try {
-            companyJsonArray = (List<Company>) jsonLoader.execute(Company.class).get();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        CompanyJSONLoader companyJSONLoader = new CompanyJSONLoader();
+        companyJSONLoader.execute();
+        CategoryJSONLoader categoryJSONLoader = new CategoryJSONLoader();
+        categoryJSONLoader.execute();
     }
 
-    public void initCategorySpinner() {
+    public void initCategorySpinner(List<Category> categories) {
         Spinner spinner = (Spinner) findViewById(R.id.category_spinner);
 
         List<String> values = new ArrayList<>();
-        if (categoryJsonArray != null) {
-            for (Category category : categoryJsonArray) {
-                values.add(category.getName());
-            }
+        for (Category category : categories) {
+            values.add(category.getName());
         }
-        ArrayAdapter<String> categories = new ArrayAdapter<>(this, R.layout.spinner, values);
-        categories.setDropDownViewResource(R.layout.spinner_item);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, R.layout.spinner, values);
+        arrayAdapter.setDropDownViewResource(R.layout.spinner_item);
 
-
-        spinner.setAdapter(categories);
+        spinner.setAdapter(arrayAdapter);
     }
 
-    public void initCompanyList() {
+    public void initCompanyList(List<Company> companies) {
         ListView listView = (ListView) findViewById(R.id.company_listView);
 
-        ListAdapter companyListAdapter = new CompanyListAdapter(this, companyJsonArray);
+        ListAdapter companyListAdapter = new CompanyListAdapter(this, companies);
 
         listView.setAdapter(companyListAdapter);
+    }
+
+
+    class CompanyJSONLoader extends AsyncTask<Void, Void, List<Company>> {
+
+        @Override
+        protected List<Company> doInBackground(Void... voids) {
+
+            return JSONLoader.loadCompanies();
+        }
+
+        @Override
+        protected void onPostExecute(List<Company> companies) {
+            initCompanyList(companies);
+        }
+    }
+
+    class CategoryJSONLoader extends AsyncTask<Void, Void, List<Category>> {
+
+        @Override
+        protected List<Category> doInBackground(Void... voids) {
+            return JSONLoader.loadCategories();
+        }
+
+        @Override
+        protected void onPostExecute(List<Category> categories) {
+            initCategorySpinner(categories);
+        }
     }
 }
